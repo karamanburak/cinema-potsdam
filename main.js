5ab4dc06ceda060fe69787e279d88800b6b07fa9
Burak Karaman <150926922+karamanburak@users.noreply.github.com>
2024-02-14 11:24:38 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,29 +3,29 @@
 //^             INDEX.HTML             */
 //^ ---------------------------------- */
 
-document.querySelector(".film1").ondblclick =() => {
-    window.location.href = "./siccin6.html"
-}
-document.querySelector(".film2").ondblclick =() => {
-    window.location.href = "./zentralGelegen.html"
-}
-document.querySelector(".film3").ondblclick =() => {
-    window.location.href = "./betweenTwoDawns.html"
-}
-document.querySelector(".film4").ondblclick =() => {
-    window.location.href = "./suursuzAsk.html"
-}
-document.querySelector(".film5").ondblclick =() => {
-    window.location.href = "./gonulDagi.html"
-    target="_blank"
-}
+document.querySelector(".film1").ondblclick = () => {
+  window.location.href = "./siccin6/siccin6.html";
+};
+document.querySelector(".film2").ondblclick = () => {
+  window.location.href = "./zentral-gelegen/zentralGelegen.html";
+};
+document.querySelector(".film3").ondblclick = () => {
+  window.location.href = "./between-two-dawns/betweenTwoDawns.html";
+};
+document.querySelector(".film4").ondblclick = () => {
+  window.location.href = "./suursuz-ask/suursuzAsk.html";
+};
+document.querySelector(".film5").ondblclick = () => {
+  window.location.href = "./gonul-dagi/gonulDagi.html";
+
+};
 
 const swiper = new Swiper(".swiper", {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: 5,
-  speed: 600,
+  speed: 500,
   coverflowEffect: {
     rotate: 30,
     stretch: 0,
